def92d35370b67110172712b59a13b56856742bc
trapadulli <trapadulli@gmail.com>
2021-09-25 17:02:28 -0400

    removed old secret

--- Library/Secrets/Azure.js
+++ Library/Secrets/Azure.js
@@ -2,8 +2,8 @@
 module.exports = {
     Secrets: function (){
         return {
-                'URL':'https://algosresourcegroupdiag.table.core.windows.net/?sv=2020-08-04&ss=t&srt=sco&sp=r&se=2030-09-25T11:36:27Z&st=2021-09-25T03:36:27Z&spr=https&sig=K61hafREJ3Yj8TpfrCq6014WYNZQK3b4DPJKWGcmQTU%3D', 
-                'SAS':  '?sv=2020-08-04&ss=t&srt=sco&sp=r&se=2030-09-25T11:36:27Z&st=2021-09-25T03:36:27Z&spr=https&sig=K61hafREJ3Yj8TpfrCq6014WYNZQK3b4DPJKWGcmQTU%3D'
-            }
+                'URL':'', 
+                'SAS':  ''
+                }
     }
  };

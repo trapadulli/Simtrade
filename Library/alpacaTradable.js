cc7766a064636cdc15ff415d3d2a6ec6ed65c3a2
trapadulli <trapadulli@gmail.com>
2021-09-26 00:24:17 -0400

    cd

---
+++ Library/alpacaTradable.js
@@ -0,0 +1,34 @@
+"use strict";
+
+function alpacaTradable(alpaca, positions, calback) {
+  var weights = {};
+  var checklist = [];
+  positions.forEach((position) => {
+    weights[position.symbol] = position.score;
+    checklist.push(position.symbol);
+  });
+
+  alpaca.getAssets().then((assets) => {
+    var positionsPrime = [];
+    assets.forEach(function (asset) {
+      if (asset.tradable && checklist.includes(asset.symbol)) {
+        if (weights[asset.symbol] < 0) {
+          if (asset.easy_to_borrow || asset.shortable) {
+            positionsPrime.push({
+              symbol: asset.symbol,
+              score: weights[asset.symbol],
+            });
+          }
+        } else {
+          positionsPrime.push({
+            symbol: asset.symbol,
+            score: weights[asset.symbol],
+          });
+        }
+        
+      }
+    });
+    calback(positionsPrime);
+  });
+}
+exports.alpacaTradable = alpacaTradable;

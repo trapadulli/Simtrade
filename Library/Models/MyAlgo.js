515850ad40825e549a4736060e91c75701df7e02
trapadulli <trapadulli@gmail.com>
2021-09-26 00:04:32 -0400

    its all good

---
+++ Library/Models/MyAlgo.js
@@ -0,0 +1,13 @@
+"use strict";
+var azure = require('azure-storage');
+const alpaca = require("../Secrets/AlpacaCreds").getCreds();
+const AzureStorage = require("../AzureStorage");
+const { AzureAccess } = require("../../Core/AzureAccess");
+
+const tableService = AzureAccess()
+module.exports = {
+run: function (printday, day, file, trade, testortrade) {
+
+  },
+};
+
